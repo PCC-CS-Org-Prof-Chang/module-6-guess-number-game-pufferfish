08a0353b6aeb7ac181f90b3a59fe08173de15911
dylantruong24 <156159572+dylantruong24@users.noreply.github.com>
2024-01-22 23:06:44 -0800

    Update GuessNumberGame.java

--- src/main/java/edu/pasadena/cs/cs03b/GuessNumberGame.java
+++ src/main/java/edu/pasadena/cs/cs03b/GuessNumberGame.java
@@ -1,13 +1,23 @@
 package edu.pasadena.cs.cs03b;
+import java.util.Scanner;
 
 public class GuessNumberGame {
 
     public static void main(String[] args) {
-        String strMsg = GuessNumberGame.dummy();
+        Scanner scanner = new Scanner (System.in);
+
+        System.out.print("Enter a number of digits: ");
+        int digitNum = scanner.nextInt();
+        System.out.print("Enter how much money you want to start with: ");
+        int moneyAmt = scanner.nextInt();
+
+        String strMsg = GuessNumberGame.dummy(digitNum, moneyAmt);
         System.out.println(strMsg);
     }
 
-    public static String dummy() {
-        return "This is a Dummy Test";
+    public static String dummy(int digitNum, int moneyAmt) {
+        int totalMoney = moneyAmt;
+
+        return "test" + digitNum;
     }
 }
