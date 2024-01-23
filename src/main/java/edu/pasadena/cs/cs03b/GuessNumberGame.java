2cc6fa75fd4eda341920ddb87f7eeddae7647d53
justintan67 <156160613+justintan67@users.noreply.github.com>
2024-01-22 23:55:56 -0800

    Guess and money input
    Prompts user for guess of number in a loop

--- src/main/java/edu/pasadena/cs/cs03b/GuessNumberGame.java
+++ src/main/java/edu/pasadena/cs/cs03b/GuessNumberGame.java
@@ -1,5 +1,8 @@
 package edu.pasadena.cs.cs03b;
 import java.util.Scanner;
+import java.lang.Math;
+
+
 
 public class GuessNumberGame {
 
@@ -9,15 +12,52 @@ public class GuessNumberGame {
         System.out.print("Enter a number of digits: ");
         int digitNum = scanner.nextInt();
         System.out.print("Enter how much money you want to start with: ");
-        int moneyAmt = scanner.nextInt();
+        int totalMoney = scanner.nextInt();
 
-        String strMsg = GuessNumberGame.dummy(digitNum, moneyAmt);
-        System.out.println(strMsg);
-    }
+        int randomNum = (int)(Math.random() * Math.pow(10, digitNum - 1));
+        String anotherGuess = "";
+        System.out.println(randomNum);
+        System.out.println(totalMoney);
 
-    public static String dummy(int digitNum, int moneyAmt) {
-        int totalMoney = moneyAmt;
+        do {
+
+            System.out.print("Enter a guess: ");
+            int guessNum = scanner.nextInt();
+            System.out.println(totalMoney);
+            if (guessNum == randomNum) {
+                totalMoney = 0;
+                System.out.println("You Win!");
+                break;
+            }
+            else if (guessNum > randomNum) {
+                System.out.println("Your guess is too high.");
+                System.out.print("Would you like to buy another guess? (Y/N)");
+                if (scanner.nextLine() == "Y") {
+                    totalMoney -= 1;
+                }
+                else {
+                    break;
+                }
+            }
+            else {
+                System.out.println("Your guess is too low.");
+                System.out.print("Would you like to buy another guess? (Y/N)");
+                if (scanner.nextLine() == "Y") {
+                    totalMoney -= 1;
+
+                }
+                else {
+                    break;
+                }
+            }
+
+        }
+        while (totalMoney > 0);
+
+
 
         return "test" + digitNum;
     }
-}
+
+    }
+        
